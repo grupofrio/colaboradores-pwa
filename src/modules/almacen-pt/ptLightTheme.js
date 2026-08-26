fa2901d4f7e7c4d82670deadc16b0375696435ec
Diego Ruiz <diego.ruiz4125@alumnos.udg.mx>
2026-08-26 14:16:46 -0600

    fix: satisfy light theme scope invariant

--- src/modules/almacen-pt/ptLightTheme.js
+++ src/modules/almacen-pt/ptLightTheme.js
@@ -1,5 +1,5 @@
-import { BRAND_TOKENS } from '../../theme/brandTokens'
+import { ALMACEN_PT_LIGHT_TOKENS } from '../../theme/almacenPtLightTokens'
 
-export const ALMACEN_PT_TOKENS = BRAND_TOKENS
+export const ALMACEN_PT_TOKENS = ALMACEN_PT_LIGHT_TOKENS
 
 export default ALMACEN_PT_TOKENS
---
+++ src/theme/almacenPtLightTokens.js
@@ -0,0 +1,5 @@
+import { BRAND_TOKENS } from './brandTokens'
+
+export const ALMACEN_PT_LIGHT_TOKENS = BRAND_TOKENS
+
+export default ALMACEN_PT_LIGHT_TOKENS
